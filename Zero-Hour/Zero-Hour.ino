b45e48778b5e0066a682d2118f7b4328c0f8cd60
iiriis <55133414+iiriis@users.noreply.github.com>
2022-06-26 11:35:24 +0530

    updated with GPS functionality and added rtos

--- Zero-Hour/Zero-Hour.ino
+++ Zero-Hour/Zero-Hour.ino
@@ -1,4 +1,5 @@
 #include "LIS3DHTR.h"
+#include <TinyGPSPlus.h>
 #include <Wire.h>
 
 
@@ -6,20 +7,42 @@
 LIS3DHTR<TwoWire> LIS;
 #define WIRE Wire
 
+TinyGPSPlus gps;
 
+TaskHandle_t t1;
+
+String lat = "INV", lon = "INV", msg = "";
 
 void setup() {
 
   Serial.begin(115200);
+  accelInit();
+  gpsInit();
+
+}
+
+void accelInit()
+{
   while (!Serial) {};
   LIS.begin(WIRE, 0x19);
   delay(100);
-
-
   LIS.setOutputDataRate(LIS3DHTR_DATARATE_5KHZ);
   LIS.setHighSolution(true);
-
 }
+
+void gpsInit()
+{
+  Serial2.begin(9600);
+  Serial.println("GPS Initialized");
+  xTaskCreatePinnedToCore(runGPS,
+                          "t1",
+                          3000,
+                          NULL,
+                          1,
+                          &t1,
+                          1);
+}
+
 
 void loop() {
   if (!LIS)
@@ -39,8 +62,29 @@ void loop() {
     Serial.println("Accident Happened");
     delay(3000);
   }
+}
 
-  Serial.print(xacl); Serial.print("  ");
-  Serial.print(yacl); Serial.print("  ");
-  Serial.println(zacl);
+void runGPS(void *pv)
+{
+
+  for (;;)
+  {
+    boolean flag = false;
+    while (Serial2.available() > 0)
+      if (gps.encode(Serial2.read()))
+        flag = true;
+
+    if (flag && gps.location.isValid())
+    {
+      lat = String(gps.location.lat(), 6);
+      lon = String(gps.location.lng(), 6);
+      msg = lat + " , " + lon;
+
+      Serial.println(msg);
+    }
+    else
+      Serial.println("Invalid");
+    delay(1000);
+  }
+
 }
